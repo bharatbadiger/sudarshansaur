a0b5c8f074d6e52eb4544743499b83d738833c0b
Akash Giri <akx.sonu@gmail.com>
2023-07-20 00:54:33 +0530

    Added swagger ui

--- src/main/java/co/bharat/sudarshansaur/SudarshansaurApplication.java
+++ src/main/java/co/bharat/sudarshansaur/SudarshansaurApplication.java
@@ -1,9 +1,12 @@
 package co.bharat.sudarshansaur;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Sudarshan Saur API", version = "0.0.1", description = "Api contract"))
 public class SudarshansaurApplication {
 
 	public static void main(String[] args) {
