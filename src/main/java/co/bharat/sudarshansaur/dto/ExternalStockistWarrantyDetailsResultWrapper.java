d0fd0d12899850f4850e0ff20f82db4542230566
bharat <bharat@bharat>
2023-11-01 12:50:11 +0530

    Ignore irrelevant properties in Response

--- src/main/java/co/bharat/sudarshansaur/dto/ExternalStockistWarrantyDetailsResultWrapper.java
+++ src/main/java/co/bharat/sudarshansaur/dto/ExternalStockistWarrantyDetailsResultWrapper.java
@@ -2,6 +2,7 @@ package co.bharat.sudarshansaur.dto;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
@@ -11,59 +12,9 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ExternalStockistWarrantyDetailsResultWrapper {
 
-	@JsonProperty("Dealer Code")
-	private String dealerCode;
-	
-	@JsonProperty("Products Deals in")
-	private String productsDealsIn;	
-	
-	@JsonProperty("Dealer Firm Name")
-	private String dealerFirmName;
-
-	@JsonProperty("Contact Person")
-	private String contactPerson;
-	
-	@JsonProperty("Sales Executive")
-	private String salesExecutive;
-
-	@JsonProperty("Applicable To Lead")
-	private String applicableToLead;
-	
-	@JsonProperty("Active")
-	private String active;
-	
-	@JsonProperty("Type")
-	private String type;
-	
-	@JsonProperty("Assigned to")
-	private String assignedTo;
-	
-	@JsonProperty("Description")
-	private String description;
-	
-	@JsonProperty("Dealers Email")
-	private String delaersEmail;	
-	
-	@JsonProperty("State")
-	private String state;
-	
-	@JsonProperty("district")
-	private String district;
-	
-	@JsonProperty("Taluka")
-	private String taluka;
-	
-	@JsonProperty("Postal Code")
-	private String postalCode;
-	
-	@JsonProperty("Marketing Area")
-	private String marketingArea;
-	
-	@JsonProperty("Address")
-	private String address;
-	
 	@JsonProperty("Serial")
 	private List<ExternalWarrantyDetailsDTO> serial;
 
