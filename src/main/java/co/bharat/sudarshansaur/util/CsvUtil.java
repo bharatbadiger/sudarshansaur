1bb955744e4f0bba4943c563722d35cc26395142
Akash Giri <akx.sonu@gmail.com>
2024-07-04 01:08:19 +0530

    Added CDM report

--- src/main/java/co/bharat/sudarshansaur/util/CsvUtil.java
+++ src/main/java/co/bharat/sudarshansaur/util/CsvUtil.java
@@ -1,6 +1,5 @@
 package co.bharat.sudarshansaur.util;
 
-import co.bharat.sudarshansaur.dto.CDMReportDTO;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 
@@ -8,48 +7,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.lang.reflect.Field;
 import java.util.List;
-
-//public class CsvUtil {
-//
-//    public static ByteArrayInputStream generateCSV(List<CDMReportDTO> list) throws IOException {
-//        ByteArrayOutputStream out = new ByteArrayOutputStream();
-//
-//        // Define CSV format with headers
-//        final CSVFormat format = CSVFormat.DEFAULT
-//                .withHeader("CDM Number", "Customer's Name", "Customer's Address", "District",
-//                        "State or Union Territory", "Customer's Phone No.", "Dealer", "Place",
-//                        "Capacity (LPD)", "Model", "Invoice Number", "Invoice Date",
-//                        "Total Qty", "Installation date", "System Serial Number");
-//
-//        try (CSVPrinter csvPrinter = new CSVPrinter(new PrintWriter(out), format)) {
-//            for (CDMReportDTO item : list) {
-//                // Print each record, ensuring each field is quoted
-//                csvPrinter.printRecord(
-//                        item.getCdmNumber(),
-//                        item.getCustomerName(),
-//                        item.getCustomerFullAddress(),
-//                        item.getCustomerDistrict(),
-//                        item.getState(),
-//                        item.getPhone(),
-//                        item.getStockistFirmName(),
-//                        item.getStockistPlace(),
-//                        item.getCapacity(),
-//                        item.getModel(),
-//                        item.getInvoiceNumber(),
-//                        item.getInvoiceDate(),
-//                        item.getQuantity(),
-//                        item.getInstallationDate(),
-//                        item.getSerialNumber()
-//                );
-//            }
-//        }
-//
-//        // Return ByteArrayInputStream containing the CSV data
-//        return new ByteArrayInputStream(out.toByteArray());
-//    }
-//}
 
 
 public class CsvUtil<T> {
@@ -78,7 +36,6 @@ public class CsvUtil<T> {
         return new ByteArrayInputStream(out.toByteArray());
     }
 
-    // Helper method to extract values from an object instance
     private Object[] extractValues(T item, int numFields) throws IllegalAccessException {
         Object[] values = new Object[numFields];
         java.lang.reflect.Field[] fields = type.getDeclaredFields();
